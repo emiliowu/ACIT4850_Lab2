30bc9f02c9e7b2d429e7fbec8bddbd4b86b69e1f
emiliowu <emilio_wu@hotmail.com>
2016-01-24 10:32:27 -0800

    Tutorial 3

--- application/config/database.php
+++ application/config/database.php
@@ -76,13 +76,13 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
+	'database' => 'ACIT 4850 Lab 3',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
+	'db_debug' => TRUE,
 	'cache_on' => FALSE,
 	'cachedir' => '',
 	'char_set' => 'utf8',
@@ -94,3 +94,5 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+$autoload['libraries'] = array('database', 'parser');
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,8 +20,27 @@ class Welcome extends Application {
 	 */
 	public function index()
 	{
-		//$this->load->view('welcome');
-                                        $this->data['pagebody'] = 'welcome';
-                                        $this->render();
+            //get all the images
+            $pix = $this->images->newest();
+            
+            // build an array of formatted cells for them
+            foreach ($pix as $picture)
+                $cells[] = $this->parser->parse('_cell', (array) $picture, true);
+            
+            // prime the table class
+            $this ->load->library('table');
+            $parms = array(
+                'table_open' => '<table class="gallery">',
+                'cell_start' => '<td class="oneimage">',
+                'cell_atl_start' => '<td class="oneimage">'
+            );
+            $this->table->set_template($parms);
+            
+            // generate the table
+            $rows = $this->table->make_columns($cells, 3);
+            $this->data['thetable'] = $this->table->generate($rows);
+            
+            $this->data['pagebody'] = 'welcome';
+            $this->render();
 	}
 }
